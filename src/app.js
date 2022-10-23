0c74c16ca5c71451791c50f6785ecfcc83ba10dd
LeandroOliver26 <leandroinfra043@gmail.com>
2022-10-23 13:47:06 -0300

    Criação das rotas e servidor

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-//instanciamos o express e utilizar a const app para usar suas funcionalidades, como criar uma rota de acordo com metodo e função e subir o servidor.
 const express = require("express");
 const CalculadoraController = require("./controller/CalculadoraController");
 const app = express();
